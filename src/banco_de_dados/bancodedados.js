346bec6999ffdeea7baed0c62c0686fb21d8e3e3
Gisele Cristina de Assis Manoel <giseledev23@gmail.com>
2023-10-14 19:41:52 -0300

    configurações iniciais

---
+++ src/banco_de_dados/bancodedados.js
@@ -0,0 +1,13 @@
+module.exports = {
+    id: 0,
+    banco: {
+        nome: 'Cubos Bank',
+        numero: '123',
+        agencia: '0001',
+        senha: 'Cubos123Bank'
+    },
+    contas: [],
+    saques: [],
+    depositos: [],
+    transferencias: []
+}
